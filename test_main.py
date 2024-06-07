5f10922c503a93b60be233231135c745a2afa3f8
Supun De Silva <sdesilva2@athabasca.edu>
2024-06-07 17:14:20 -0600

    Adds linting to the workflow

---
+++ test_main.py
@@ -0,0 +1,11 @@
+import unittest
+from main import add
+
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
